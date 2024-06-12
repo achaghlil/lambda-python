5f631e0c71f76f5d742b6bdebe2df25870e0312b
Ammar <achaghlil@email.com>
2024-06-12 13:32:55 +0100

    Update Main

--- python/hello-python.py
+++ python/hello-python.py
@@ -1,7 +1,9 @@
 def lambda_handler(event, context):
    message = 'Hello {} !'.format(event['key1'])
    return {
-       'message' : message
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(message)
    }
 
    

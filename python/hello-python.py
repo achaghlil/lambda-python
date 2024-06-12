fd5946e2189e8cf219d752624fe7ecaeefe642fb
Ammar <achaghlil@email.com>
2024-06-12 14:56:51 +0100

    Update Function Logic

--- python/hello-python.py
+++ python/hello-python.py
@@ -1,10 +1,17 @@
 import json;
 
 def lambda_handler(event, context):
-   price = int(event['price'])
-   discount = int(event['discount'])
-   result = price*(1-discount/100)
-
+    if 'price' in event and 'discount' in event:
+        price = int(event['price'])
+        discount = int(event['discount'])
+        result = price*(1-discount/100)
+        result = f'The discounted price is : {result}'
+    else:
+        result = '''You Should Pass the following parameters as json 
+        "price" with number 
+        "discount" with number like 50 for 50 percent discount
+        '''
+        
    return {
        'statusCode' : 200,
        'body' : json.dumps(result)

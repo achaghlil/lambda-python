c87dd768a8d48a52cdbdc6e41cb4b87e3ab61603
Ammar <achaghlil@email.com>
2024-06-12 14:19:19 +0100

    Update

--- python/hello-python.py
+++ python/hello-python.py
@@ -4,10 +4,10 @@ def lambda_handler(event, context):
    price = int(event['price'])
    discount = int(event['discount'])
    result = price*(1-discount/100)
-   
+
    return {
        'statusCode' : 200,
-       'body' : 
+       'body' : json.dumps(result)
    }
 
    
